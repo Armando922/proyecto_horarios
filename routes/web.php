2ae91ba01d22aa1f30bc3da4354423509b07d1ba
miguelasch <miguel.angel.sunagua@gmail.com>
2026-07-03 09:12:03 -0400

    fix: elimina rutas de desarrollo /hola y /home_layout

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home_layout', function () {
-    return view('layout');
-});
-Route::get("/hola",function(){
-    return "Hola mundo";
-});
+
 
 Route::get('/subjects', function () {
     return view('subjects.index');
